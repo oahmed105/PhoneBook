9ffd836c7aca1f0f72c9d47683cd12fa7aa356f9
oahmed105 <oahmed10598@gmail.com>
2023-08-05 13:02:45 -0400

    making progress

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -24,23 +21,57 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        if (!hasEntry(name)) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(phoneNumber);
+            phonebook.put(name, list);
+
+        }
+        phonebook.get(name).add(phoneNumber);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        if (!hasEntry(name)) {
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(List.of(phoneNumbers));
+            phonebook.put(name, list);
+
+        }
+        phonebook.get(name).addAll(List.of(phoneNumbers));
     }
 
     public void remove(String name) {
+
     }
 
-    public Boolean hasEntry(String name) {
-        return null;
+    public Boolean hasEntry(String name, String phoneNumber) {
+        if (name  == null || phoneNumber == null){
+            return false;
+        }
+        return phonebook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        String result = "";
+        if (phonebook.containsValue(phoneNumber)){
+            for (Map.Entry<String, List<String>> entry: phonebook.entrySet()){
+                if (Objects.equals(entry.getValue(), phoneNumber)){
+                    result += entry.getKey();
+                }
+            }
+        }
+//        return result;
+//        ArrayList<String> temp = new ArrayList<>();
+//        for (Map.Entry<String, List<String>> pn: phonebook.entrySet()){
+//            if (Objects.equals(phoneNumber, pn.getValue())){
+//                temp.add(pn.getKey());
+//            }
+//        }
+//        return String.valueOf(temp);
         return null;
     }
 
